be6dbf1ba61cda7fc4a1cea04991ce838725e061
jorgeamadrid <jorgeamadrid@gmail.com>
2025-02-26 22:00:44 -0500

    Delete grey directory

--- grey/scriptlibre.py
+++
@@ -1,5 +0,0 @@
-with open("datos.json", "rb") as f:
-    contenido = f.read().decode("utf-16")  # Intenta decodificarlo como UTF-16
-
-with open("archivo_corregido.json", "w", encoding="utf-8") as f:
-    f.write(contenido)
