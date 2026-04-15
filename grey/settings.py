358b7b3bbc3d531deee540ca349bcda84aad8edb
jorgeamadrid <jorgeamadrid@gmail.com>
2026-04-14 20:22:28 -0500

    deploy render

--- grey/settings.py
+++ grey/settings.py
@@ -31,10 +31,12 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
-DEBUG = os.getenv('DEBUG') == 'True'
+#DEBUG = os.getenv('DEBUG') == 'True'
+#ALLOWED_HOSTS = ['*']
 
-ALLOWED_HOSTS = ['*']
-
+#Debug
+DEBUG = False
+ALLOWED_HOSTS = ['.onrender.com']
 
 # Application definition
 
