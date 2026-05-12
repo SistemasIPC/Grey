96c775c4b281bc9fb5974b16bb84a57ad326c847
jorgeamadrid <jorgeamadrid@gmail.com>
2026-05-12 10:47:56 -0500

    Enable media serving

--- grey/views.py
+++ grey/views.py
@@ -16,12 +16,12 @@ def media_debug(request, path):
 
     archivo = os.path.join(settings.MEDIA_ROOT, path)
 
-    print("===================================")
-    print("MEDIA REQUEST")
-    print("PATH:", path)
-    print("ARCHIVO:", archivo)
-    print("EXISTE:", os.path.exists(archivo))
-    print("===================================")
+#    print("===================================")
+#    print("MEDIA REQUEST")
+#    print("PATH:", path)
+#    print("ARCHIVO:", archivo)
+#    print("EXISTE:", os.path.exists(archivo))
+#    print("===================================")
 
     if os.path.exists(archivo):
         return FileResponse(open(archivo, "rb"))
