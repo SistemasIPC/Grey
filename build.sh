d64b6d537738a581f7980575a514fa5fae24fad9
jorgeamadrid <jorgeamadrid@gmail.com>
2026-05-11 19:41:17 -0500

    deploy render modulos

--- build.sh
+++ build.sh
@@ -2,3 +2,9 @@
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+# Crear carpeta media persistente
+mkdir -p /var/data/media
+
+# Copiar media inicial desde GitHub hacia el Disk
+cp -r media/* /var/data/media/
